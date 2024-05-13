a10f01a584134ac358c12c9ea065d0124c89ea5d
krizhnaa <krizhnaprem@gmail.com>
2024-05-13 13:00:27 +0530

    User enters 'X' and 'O', turn by turn

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 
-ttt_list = [["0", "0", "0" ],
-            ["0", "0", "0" ],
-            ["0", "0", "0" ]]
+ttt_list = [[" ", " ", " " ],
+            [" ", " ", " " ],
+            [" ", " ", " " ]]
 
-ttt_list[0][2] = "X"
-ttt_list[1][1] = "X"
-ttt_list[2][0] = "X"
+game_on = True
 
-#Display
-for i, row in enumerate(ttt_list):
-    for j, item in enumerate(row):
-        if j == 2:
-            print(item, end=" ")
-        else:
-            print(item, end=" | ")
+def display():
+    count = 0
+    print("    (0) (1) (2)")
     print()
-    if i == 2:
+    for i, row in enumerate(ttt_list):
+        for j, item in enumerate(row):
+            if j == 0:
+                print(f"({count}) ", end=" ")
+                count += 1
+
+            if j == 2:
+                print(item, end=" ")
+            else:
+                print(item, end=" | ")
         print()
-    else:
-        print("----------")
+        if i == 2:
+            print()
+        else:
+            print("     ---------")
+    return ttt_list
+
+
+
+while game_on:
+
+    display()
+
+    user_input = input("Enter the Position to place 'X' : ")
+    frst_coord = int(user_input[0])
+    scnd_coord = int(user_input[1])
+
+    ttt_list[frst_coord][scnd_coord] = "X"
+
+    display()
+
+    user_input = input("Enter the Position to place 'O' : ")
+    frst_coord = int(user_input[0])
+    scnd_coord = int(user_input[1])
+
+    ttt_list[frst_coord][scnd_coord] = "O"
+
+    display()
+
 
 
 
