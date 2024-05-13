d083d383cfef2972f89d57de143d4134e2aa737a
krizhnaa <krizhnaprem@gmail.com>
2024-05-13 12:26:01 +0530

    Fixed the Display

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 
-ttt_list = [[" ", " ", " " ],
-            [" ", " ", " " ],
-            [" ", " ", " " ]]
+ttt_list = [["0", "0", "0" ],
+            ["0", "0", "0" ],
+            ["0", "0", "0" ]]
 
 ttt_list[0][2] = "X"
+ttt_list[1][1] = "X"
+ttt_list[2][0] = "X"
 
 #Display
-for row in ttt_list:
-    for col in range(len(row)):
-        print(f"{row[col]} |")
+for i, row in enumerate(ttt_list):
+    for j, item in enumerate(row):
+        if j == 2:
+            print(item, end=" ")
+        else:
+            print(item, end=" | ")
+    print()
+    if i == 2:
+        print()
+    else:
+        print("----------")
+
+
+
 
